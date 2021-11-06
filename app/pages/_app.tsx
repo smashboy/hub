73c165b904ded0b80121ab7c30d167c7f0908418
Default <sergeitchaban@gmail.com>
2021-11-06 12:44:20 +0200

    members manager confirm dialog

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -11,41 +11,50 @@ import { GlobalStyles, ThemeProvider } from "@mui/material"
 import AdapterDateFns from "@mui/lab/AdapterDateFns"
 import LocalizationProvider from "@mui/lab/LocalizationProvider"
 import { SnackbarProvider } from "notistack"
-import lightTheme from "app/core/theme/lightTheme"
+// import lightTheme from "app/core/theme/lightTheme"
 import darkTheme from "app/core/theme/darkTheme"
+import { ConfirmDialogProvider } from "react-mui-confirm"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <SnackbarProvider maxSnack={3}>
-          <ErrorBoundary
-            FallbackComponent={RootErrorFallback}
-            onReset={useQueryErrorResetBoundary().reset}
-          >
-            <GlobalStyles
-              styles={{
-                body: { margin: 0, backgroundColor: "#121212" },
-                "::-webkit-scrollbar": {
-                  width: 4,
-                  height: 4,
-                },
+      <ConfirmDialogProvider
+        confirmTextFieldProps={{
+          variant: "outlined",
+          size: "small",
+        }}
+        cancelButtonProps={{ color: "secondary" }}
+      >
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <SnackbarProvider maxSnack={3}>
+            <ErrorBoundary
+              FallbackComponent={RootErrorFallback}
+              onReset={useQueryErrorResetBoundary().reset}
+            >
+              <GlobalStyles
+                styles={{
+                  body: { margin: 0, backgroundColor: "#121212" },
+                  "::-webkit-scrollbar": {
+                    width: 4,
+                    height: 4,
+                  },
 
-                "::-webkit-scrollbar-track": {
-                  backgroundColor: "transparent",
-                },
+                  "::-webkit-scrollbar-track": {
+                    backgroundColor: "transparent",
+                  },
 
-                "::-webkit-scrollbar-thumb": {
-                  background: "#ffffff",
-                },
-              }}
-            />
-            {getLayout(<Component {...pageProps} />, pageProps)}
-          </ErrorBoundary>
-        </SnackbarProvider>
-      </LocalizationProvider>
+                  "::-webkit-scrollbar-thumb": {
+                    background: "#ffffff",
+                  },
+                }}
+              />
+              {getLayout(<Component {...pageProps} />, pageProps)}
+            </ErrorBoundary>
+          </SnackbarProvider>
+        </LocalizationProvider>
+      </ConfirmDialogProvider>
     </ThemeProvider>
   )
 }
